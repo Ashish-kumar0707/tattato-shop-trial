ed22159fba7248275f1a470978336c28cb3bf20f
Ashish-kumar0707 <ashishkumar6378269775@gmail.com>
2023-05-05 17:52:22 +0530

    logo added and  toggle functionality added

---
+++ js/toogle.js
@@ -0,0 +1,50 @@
+
+// toggle function for best tattoos
+function toggle1(){
+  var x= document.getElementById('text1');
+  if (x.style.display === "none") {
+
+    x.style.display = "block";
+    document.getElementById("btn1").innerHTML = "Read Less";
+  } else {
+    x.style.display = "none";
+    document.getElementById("btn1").innerHTML = "Read More";
+  }
+}
+
+var a= document.getElementById('text1');
+a.style.display = "none";
+
+function toggle1(){
+  var x= document.getElementById('text1');
+  if (x.style.display === "none") {
+
+    x.style.display = "block";
+    document.getElementById("btn1").innerHTML = "Read Less";
+  } else {
+
+    x.style.display = "none";
+    document.getElementById("btn1").innerHTML = "Read More";
+
+
+  }
+}
+
+var b= document.getElementById('text2');
+b.style.display = "none";
+
+function toggle2(){
+  var x= document.getElementById('text2');
+  if (x.style.display === "none") {
+
+    x.style.display = "block";
+    document.getElementById("btn2").innerHTML = "Read Less";
+  } else {
+
+    x.style.display = "none";
+    document.getElementById("btn2").innerHTML = "Read More";
+
+
+  }
+}
+
