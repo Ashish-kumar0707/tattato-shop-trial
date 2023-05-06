ad533b7c8d22dda8c6c825ca53687d32fcb2c631
Ashish-kumar0707 <ashishkumar6378269775@gmail.com>
2023-05-07 01:07:32 +0530

    script added to the js file

--- js/toogle.js
+++ js/toogle.js
@@ -48,3 +48,21 @@ function toggle2(){
   }
 }
 
+// <----------------------------------------menu toggle------------------------------------------------>
+
+
+var menuitem = document.getElementById("menuitem");
+menuitem.style.maxHeight = "0px";
+
+function menutoggle() {
+  if (menuitem.style.maxHeight == '0px') {
+    menuitem.style.maxHeight = "200px"
+  } else {
+    menuitem.style.maxHeight = "0px"
+  }
+}
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
